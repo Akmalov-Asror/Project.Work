1d1575852542dfe0ecd80bc91231567b4e1d0165
A3S4ROBOY <110540334+Akmalov-Asror@users.noreply.github.com>
2023-02-04 20:48:15 +0500

    news

--- Project.Work/Controllers/ItemsController.cs
+++ Project.Work/Controllers/ItemsController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Diagnostics.CodeAnalysis;
+using Microsoft.AspNetCore.Mvc;
 using Project.Work.Dtos;
 using Project.Work.Inerfaces;
 using Project.Work.Services;
@@ -7,25 +8,48 @@ namespace Project.Work.Controllers;
 
 public class ItemsController : Controller
 {
-    private readonly ExpenseService _expenseService;
+    private readonly IExpenseService _expenseService;
 
-    public ItemsController(ExpenseService expenseService)
+    public ItemsController(IExpenseService expenseService)
     {
         _expenseService = expenseService;
     }
 
     public async Task<IActionResult> Index()
     {
-       var date =  await _expenseService.GetExpensiveAsync();
+       var date =  await _expenseService.GetExpensesAsync();
 
        return View(date);
     }
 
     public async Task<IActionResult> CreateItems(CreateCategoryDto createCategoryDto)
     {
+        
         if (!ModelState.IsValid)
             return View();
+
         var data = await _expenseService.CreateExpensiveAsync(createCategoryDto);
         return RedirectToAction("Index");
+    }
+
+    public async Task<IActionResult> DeleteItems(Guid Id)
+    {
+        if (!ModelState.IsValid)
+        {
+            return View();
+        }
+        var data = _expenseService.DeleteExpensiveAsync(Id);
+        return  RedirectToAction("Index");
+    }
+
+    public async Task<IActionResult> UpdateItems(UpdateItemDto updateItemDto)
+    {
+        if (!ModelState.IsValid)
+        {
+            return View();
+        }
+
+        var data = _expenseService.UpdateItemAsync(updateItemDto);
+        return View();
     }
 }
